726c6ba63bbc250eff0f4fab27230d9adad02770
Divyam Oza <divoza1103@gmail.com>
2025-02-26 16:48:41 +0530

    Added get all user.

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -1,14 +1,14 @@
 package controllers;
 
+import entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import requests.UserRequest;
 import services.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -26,4 +26,16 @@ public class UserController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers(){
+        try{
+            List<User> users = userService.getAllUsers();
+            return new ResponseEntity<>(users , HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(null , HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -3,11 +3,13 @@ package services;
 import convertor.UserConvertor;
 import entities.User;
 import exceptions.UserAlreadyExists;
+import exceptions.UserDoesNotExists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repositories.UserRepository;
 import requests.UserRequest;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,7 +17,6 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
-
 
     public String addNewUser(UserRequest userRequest) {
         User userOpt = userRepository.findByEmailId(userRequest.getEmailId());
@@ -27,6 +28,15 @@ public class UserService {
         userRepository.save(user);
 
         return "User created successfully";
+    }
+
+    public List<User> getAllUsers() {
+
+        List<User> userList = userRepository.findAll();
+        if (userList.isEmpty()) {
+            throw new UserDoesNotExists();
+        }
+        return userList;
     }
 
 }
